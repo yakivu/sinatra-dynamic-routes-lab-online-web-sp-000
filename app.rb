6432f8d6671e0eb30ef886e24f31ecdd13dfd567
Adam Walker <adamw207@yahoo.com>
2021-04-03 16:51:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
-    @phrase = params[:phrase]
+    @phrase = params[:phrase].to_s
     erb :say
   end
 
