54d2f924a31fa611956242137a691eea0454a21c
Adam Walker <adamw207@yahoo.com>
2021-04-03 17:00:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do
-    @name == params[:name]
+    @name = params[:name]
     erb :reversename
     
   end
